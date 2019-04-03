623bb669fc15ed3ce0b09580073f69bf7b87a60f
Vanessa da Silva Pereira <vanessa.pereira787@gmail.com>
2019-04-03 16:56:05 -0300

    feita a tela de configurações e criado o banco de dados

--- app/src/main/java/com/pedidotech/apprestaurante/menu.java
+++ app/src/main/java/com/pedidotech/apprestaurante/menu.java
@@ -11,6 +11,7 @@ public class menu extends AppCompatActivity {
     private Button botaoSaladas;
     private Button botaoPratoPrincipal;
     private Button botaoSobremesas;
+    private Button botaoConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +51,15 @@ public class menu extends AppCompatActivity {
         botaoSobremesas.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(menu.this, sobremesas.class);
+                startActivity(it);
+            }
+        });
+
+        botaoConfig = (Button) findViewById(R.id.btnConfig);
+        botaoConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(menu.this, config.class);
                 startActivity(it);
             }
         });
